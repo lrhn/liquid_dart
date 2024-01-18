5d7d43eed01f8963859a31d4c9841a6cc958a06a
Lasse R.H. Nielsen <lrn@google.com>
2024-01-18 09:37:18 +0100

    Tweak RegExps

--- lib/src/parser/lexer.dart
+++ lib/src/parser/lexer.dart
@@ -4,15 +4,16 @@ import '../model.dart';
 
 class _TokenCreator {
   final TokenType type;
-  final RegExp pattern;
+  final Pattern pattern;
 
-  _TokenCreator(this.type, String pattern) : pattern = RegExp(pattern, dotAll: true);
+  _TokenCreator(this.type, this.pattern);
+  _TokenCreator.re(this.type, String pattern) : pattern = RegExp(pattern, dotAll: true);
 
   Token? scan(Source source, LineScanner ss) {
     final line = ss.line;
     final column = ss.column;
     if (ss.scan(pattern)) {
-      return Token(type, ss.lastMatch!.group(0)!, source: source, line: line, column: column);
+      return Token(type, ss.lastMatch![0]!, source: source, line: line, column: column);
     }
     return null;
   }
@@ -20,25 +21,26 @@ class _TokenCreator {
 
 class Lexer {
   final List<_TokenCreator> tokenCreators = [
-    _TokenCreator(TokenType.comparison, r'==|!=|<>|<=?|>=?'),
-    _TokenCreator(TokenType.single_string, r"'[^\']*'"),
-    _TokenCreator(TokenType.double_string, r'"[^\"]*"'),
-    _TokenCreator(TokenType.number, r'-?\d+(\.\d+)?'),
-    _TokenCreator(TokenType.identifier, r'[a-zA-Z_][\w-]*\??'),
-    _TokenCreator(TokenType.dotdot, r'\.\.'),
-    _TokenCreator(TokenType.pipe, r'\|'),
-    _TokenCreator(TokenType.dot, r'\.'),
+    _TokenCreator.re(TokenType.comparison, r'[=!]=|<[=>]?|>=?'),
+    _TokenCreator.re(TokenType.single_string, r"'.*?'"),
+    _TokenCreator.re(TokenType.double_string, r'".*?"'),
+    _TokenCreator.re(TokenType.number, r'-?\d+(?:\.\d+)?'),
+    _TokenCreator.re(TokenType.identifier, r'[a-zA-Z_][\w-]*\??'),
+    _TokenCreator(TokenType.dotdot, '..'),
+    _TokenCreator(TokenType.pipe, '|'),
+    _TokenCreator(TokenType.dot, '.'),
     _TokenCreator(TokenType.assign, '='),
     _TokenCreator(TokenType.colon, ':'),
     _TokenCreator(TokenType.comma, ','),
-    _TokenCreator(TokenType.open_square, r'\['),
+    _TokenCreator(TokenType.open_square, '['),
     _TokenCreator(TokenType.close_square, ']'),
-    _TokenCreator(TokenType.open_banana, r'\('),
-    _TokenCreator(TokenType.close_banana, r'\)'),
-    _TokenCreator(TokenType.question, r'\?'),
+    _TokenCreator(TokenType.open_banana, '('),
+    _TokenCreator(TokenType.close_banana, ')'),
+    _TokenCreator(TokenType.question, '?'),
     _TokenCreator(TokenType.dash, '-'),
   ];
-  final markup = _TokenCreator(TokenType.markup, r'((?!{{)(?!{%)(?![\s\n\r]*{[{%]-).)+');
+
+  final markup = _TokenCreator.re(TokenType.markup, r'(?:[^\s{]|\{(?![{%])|\s+?(?!\s|\{[{%]-))+');
   final whitespace = RegExp(r'\s*');
 
   Iterable<Token> tokenize(Source source) sync* {
@@ -57,8 +59,8 @@ class Lexer {
     }
   }
 
-  RegExp tagStart = RegExp(r'({%-?)|([\s\n\r]*{%-)');
-  RegExp tagEnd = RegExp(r'(%})|(-%}[\s\n\r]*)');
+  RegExp tagStart = RegExp(r'\{%-?|\s+\{%-');
+  RegExp tagEnd = RegExp(r'%\}|-%\}\s*');
   Iterable<Token> tokenizeTag(Source source, LineScanner ss) => tokenizeNonMarkup(
         source,
         ss,
@@ -68,8 +70,8 @@ class Lexer {
         tagEnd,
       );
 
-  RegExp varStart = RegExp(r'({{-?)|([\s\n\r]*{{-)');
-  RegExp varEnd = RegExp(r'(}})|(-}}[\s\n\r]*)');
+  RegExp varStart = RegExp(r'\{\{-?|\s+\{\{-');
+  RegExp varEnd = RegExp(r'\}\}|-\}\}\s*');
   Iterable<Token> tokenizeVar(Source source, LineScanner ss) => tokenizeNonMarkup(
         source,
         ss,
@@ -84,8 +86,9 @@ class Lexer {
     yield Token(startType, ss.lastMatch!.group(0)!, source: source, line: ss.line, column: ss.column - 2);
 
     mainLoop:
-    while (ss.scan(whitespace) && !ss.matches(end)) {
+    while (true) {
       ss.scan(whitespace);
+      if (ss.scan(end)) break;
 
       for (final creator in tokenCreators) {
         final token = creator.scan(source, ss);
@@ -95,11 +98,11 @@ class Lexer {
         }
       }
 
-      // if we get here then we didn't match a token
+      // if we get here then we didn't match a token, or end,
+      // so this `expect` call will throw.
       ss.expect(end);
     }
 
-    ss.expect(end);
-    yield Token(endType, ss.lastMatch!.group(0)!, source: source, line: ss.line, column: ss.column - 2);
+    yield Token(endType, ss.lastMatch![0]!, source: source, line: ss.line, column: ss.column - 2);
   }
 }
